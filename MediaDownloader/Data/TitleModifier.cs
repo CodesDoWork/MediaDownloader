2cb348b698eb352823a1ac2fb845ac82b3f51028
Justin Konratt <codesdowork@gmail.com>
2022-01-10 12:08:22 +0100

    Added new method for distributing default  title modifiers

--- MediaDownloader/Data/TitleModifier.cs
+++ MediaDownloader/Data/TitleModifier.cs
@@ -1,12 +1,23 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
+using System.Linq;
 using static MediaDownloader.Data.DatabaseContext;
 
 namespace MediaDownloader.Data
 {
     public class TitleModifier
     {
+        public static readonly List<TitleModifier> DefaultModifiers = new()
+        {
+            new TitleModifier {Target = "[\\[\\(][Oo]fficial[Aa]udio[\\]\\)]"},
+            new TitleModifier {Target = "[\\[\\(][Oo]riginal [Aa]udio[\\]\\)]"},
+            new TitleModifier {Target = "[\\[\\(][Oo]fficial [Vv]ideo[^\\]\\)]*[\\]\\)]"},
+            new TitleModifier {Target = "[\\[\\(][Oo]fficial [Mm]usic [Vv]ideo[^\\]\\)]*[\\]\\)]"},
+            new TitleModifier {Target = "[\\[\\(][Ll]yrics? [Vv]ideo[^\\]\\)]*[\\]\\)]"}
+        };
+
         [Key] public int Id { get; set; }
 
         public string Target { get; set; }
@@ -16,7 +27,18 @@ namespace MediaDownloader.Data
         public static ObservableCollection<TitleModifier> GetLocalSavedDownloads()
         {
             DBConnection.TitleModifiers.Load();
-            return DBConnection.TitleModifiers.Local;
+            var result = DBConnection.TitleModifiers.Local;
+
+            foreach (var defaultModifier in DefaultModifiers)
+            {
+                if (result.All(modifier => modifier.Target != defaultModifier.Target))
+                {
+                    DBConnection.TitleModifiers.Add(defaultModifier);
+                }
+            }
+
+            DBConnection.SaveChanges();
+            return result;
         }
     }
 }
